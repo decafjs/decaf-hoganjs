e084c33e78377a5347e4dfd1146d1521149baa7b
Michael Schwartz <mykesx@gmail.com>
2014-05-21 10:51:54 -0700

    Added TemplateManager

--- index.js
+++ index.js
@@ -6,5 +6,46 @@
  * To change this template use File | Settings | File Templates.
  */
 
-module.exports = require('lib/hogan.js');
+var Hogan = require('lib/hogan');
+
+function TemplateManager( path ) {
+	var templates = {},
+	    template_path = path;
+
+    if (!template_path.endsWith('/')) {
+        template_path += '/';
+    }
+    return new JavaAdapter(org.mozilla.javascript.NativeObject, {
+        get : sync(function( name, start ) {
+            if (String(name) === 'stackText') {
+                return undefined;
+            }
+            var path = template_path + name + '.hbs',
+                t = templates[path];
+            if (!t) {
+                t = templates[path] = {
+                    path     : path,
+                    file     : new File(path),
+                    modified : 0
+                };
+            }
+            var f = t.file;
+            if (!f.isFile()) {
+                throw new Error('View ' + path + ' not found');
+            }
+            var modified = f.lastModified();
+            if (modified > t.modified) {
+                t.template = Hogan.compile(f.readAll());
+                t.modified = modified;
+            }
+            return t.template;
+        }, templates)
+    });
+}
+
+module.exports = {
+	Hogan: Hogan,
+	TemplateManager: TemplateManager
+};
+
 
